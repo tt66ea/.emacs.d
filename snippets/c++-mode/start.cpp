048a717962be17f40ea69b1561d2c81694626daa
tt66ea <tt66ea@qq.com>
2026-09-07 21:08:08 +0800

    Track snippets: remove snippets/ from .gitignore

---
+++ snippets/c++-mode/start.cpp
@@ -0,0 +1,12 @@
+# -*- mode: snippet -*-
+# name: start
+# key: start
+# --
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int main() {
+    return 0;
+}
